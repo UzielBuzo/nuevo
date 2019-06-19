1ff0aed57fbd345126d0b88f50b6ce70a53a0547
Uziel Buzo <buzodancing@gmail.com>
2019-06-18 18:02:23 -0600

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+//importando componentes de la ruta
+import { HomeComponent } from './home/home.component';
+import { ModelsComponent } from './models/models.component';
+import { ServicesComponent } from './services/services.component'
 
-const routes: Routes = [];
+
+//rutas de la aplicacion
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'models', component: ModelsComponent},
+  {path: 'services', component: ServicesComponent},
+  {path: '**', component: HomeComponent}, //este es para cuando alguien se pase de lanza lo regrese al home
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
